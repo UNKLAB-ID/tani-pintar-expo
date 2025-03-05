cd46bcd24663a5528c1f6fffc4f713b305e580eb
putrasamb <putra.mahardika.17@gmail.com>
2025-03-05 22:12:52 +0700

    feat: Add global CSS for Tailwind integration and update dependencies

--- app.tsx
+++ app.tsx
@@ -2,6 +2,7 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Slot } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import "../global.css";
 
 export default function App() {
     return (
