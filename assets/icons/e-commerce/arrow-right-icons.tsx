dd0f255def56c351f33ebc1a9770d28ca92557e5
nabilhatami86 <rafamonton6@gmail.com>
2025-07-13 15:51:42 +0700

    fix: reviews

--- assets/icons/e-commerce/arrow-right-icons.tsx
+++ assets/icons/e-commerce/arrow-right-icons.tsx
@@ -2,14 +2,7 @@ import * as React from 'react';
 import Svg, { SvgProps, Path } from 'react-native-svg';
 
 const ArrowRightIcons = ({ width = 24, height = 24, ...props }: SvgProps) => (
-  <Svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    width={width}
-    height={height}
-    {...props}
-  >
+  <Svg fill="none" viewBox="0 0 24 24" width={width} height={height} {...props}>
     <Path
       fill="#1F1F1F"
       fillRule="evenodd"
